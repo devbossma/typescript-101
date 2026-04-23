83c6a7bd602c0c179e5f0977cb8f163c5b8f57ee
devbossma <ysaber201@outlook.com>
2026-04-24 00:49:54 +0100

    add: Type Assertions, Interfaces, Object Types

--- types.ts
+++ types.ts
@@ -38,18 +38,180 @@ function add(x: number, y: number): number {
 
 // ========================================
 
-// Declared or return Types ones could be buit in or customized.
+// Declared or return Types ones could be buit in or customized (Type Aliases).
 async function getFavoriteNumber(): Promise<number> {
     return 26;
 }
 
-// Costome Types
+// Type Aliases
 type ResponseMessage = { code: number, message: string }
 
 async function HttpCustomResponse(c: number, m: string): Promise<ResponseMessage> {
     return { code: c, message: m }
 }
 
+
+// Object Types
+// ============
+
+// The parameter's type annotation is an object type
+function printCoordinations(point: { x: number; y: number }) {
+    //                              ^^^^^^^^^^^^^^^^^^^^^^^^
+    // (parameter) point: {
+    //     x: number;               <== Type Object
+    //     y: number;
+    // }
+    console.log("The coordinate's x value is " + point.x);
+    console.log("The coordinate's y value is " + point.y);
+}
+printCoordinations({ x: 3, y: 7 });
+// or 
+let point = { x: 5, y: 65 }
+printCoordinations(point)
+
+// Optional Properties
+
+function greting(name?: string): void {
+    // if (!name) {
+    //     console.log("Hello guest!")
+    // } else {
+    //     console.log(`Hello ${name}!`)
+    // }
+    !name ? console.log("Hello guest!") : console.log(`Hello ${name}!`)
+
+}
+greting() // <== "Hello guest!"
+greting("Yassine") // "Hello Yassine!"
+
+
+// Union Types
+
+function returnCode(
+    code:
+        string
+        | number
+        | boolean) {
+    return code
+}
+
+// Bothe provided code Type Correct
+returnCode("78249827")
+returnCode(78249827)
+
+// Assigne a union to a Type Aliase
+
+type returned = string | number | boolean
+
+function returnstatus(code: number | string): returned {
+    if (code === "200" || code === 200) {
+        return 1
+    }
+    if (code === 201 || code === "201") {
+        return "created"
+    }
+    if (code === "404" || code === 404) {
+        return "Not Found"
+    }
+
+    return false
+}
+
+console.log(returnstatus(404))   // Not Found
+console.log(returnstatus("404")) // Not Found
+console.log(returnstatus(500))   // false
+console.log(returnstatus("201")) // created
+console.log(returnstatus(200))   // 1
+
+// Union Problem:
+// function printId(id: number | string) {
+//     console.log(id.toUpperCase());
+
+
+//     // Property 'toUpperCase' does not exist on type 'string | number'.
+//     //         Property 'toUpperCase' does not exist on type 'number'.
+// }
+
+// The solution is to narrow the union with code:
+
+function printId(id: number | string) {
+    if (typeof id === "string") {
+        // In this branch, id is of type 'string'
+        console.log(id.toUpperCase());
+    } else {
+        // Here, id is of type 'number'
+        console.log(id);
+    }
+}
+
+
+function welcomePeople(x: string[] | string) {
+    if (Array.isArray(x)) {
+        // Here: 'x' is 'string[]'
+        console.log("Hello, " + x.join(" and "));
+    } else {
+        // Here: 'x' is 'string'
+        console.log("Welcome lone traveler " + x);
+    }
+}
+
+
+
+// Interfaces
+// An interface declaration is another way to name an object type:
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function printCoord(pt: Point) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 100, y: 100 });
+
+// Extending an interface
+
+interface User {
+    name: string
+    email: string
+}
+
+interface Admin extends User {
+    role: string
+}
+
+function getAdminInfo(user: Admin) {
+    return user;
+}
+
+const u1 = getAdminInfo({ name: "Yassine", email: "Y@g.c", role: "admin" })
+console.log(u1.name)
+
+// Extending a type via intersections
+
+type UserType = {
+    name: string
+    email: string
+}
+
+// Also String Literal Type are allowed as Type Aliase 
+type Role = "user" | "admin"
+type AdminType = UserType & { role: Role }
+
+function getAdminTypeInfo(admin: AdminType): void {
+    console.log(admin.role)
+}
+
+getAdminTypeInfo({ name: "Yassine", email: "Y@g.c", role: "admin" })
+// getAdminTypeInfo({ name: "Yassine", email: "Y@g.c", role: "guest" }) // Type '"guest"' is not assignable to type 'Role'.
+
+
+
+// Type Assertions
+// Sometimes you will have information about the type of a value that TypeScript can’t know about.
+const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
 
 
 
